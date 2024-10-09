5aa890f84aa38b1fb6f34b55218bf69d0ea82bb3
BoissonMathis <math.boiss@laposte.net>
2024-10-09 19:03:18 +0200

    création des service.controller

--- src/service/app/service.controller.ts
+++ src/service/app/service.controller.ts
@@ -1,9 +1,55 @@
-import { Controller } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    NotFoundException,
+    Param,
+    Post,
+    BadRequestException,
+} from '@nestjs/common'
 import { ServiceService } from './service.service'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiResponse, ApiTags } from '@nestjs/swagger'
+import { Service } from '../domaine/service.entity'
+import { CreateServiceDto } from './create-service.dto'
+import { HttpExceptionResponse } from '../../shared/exception-response/http-exception-response'
 
 @ApiTags('Service Controller')
 @Controller('service')
 export class ServiceController {
     constructor(private readonly serviceService: ServiceService) {}
+
+    @Get(':ids')
+    @ApiResponse({ status: 200, description: "Renvoie les services correspondant aux ID's", type: Service })
+    @ApiResponse({
+        status: 404,
+        type: HttpExceptionResponse,
+        description: `${NotFoundException.name} => Aucun service correspondants aux ID's`,
+    })
+    async getServiceById(@Param('ids') serviceIds: number[]) {
+        const services = await this.serviceService.getServices(serviceIds)
+        return services.length ? services : null
+    }
+
+    @Post('create')
+    @ApiResponse({ status: 201, description: 'Renvoie le service créé', type: Service })
+    @ApiResponse({
+        status: 400,
+        type: HttpExceptionResponse,
+        description: `${BadRequestException.name} => Si l'utilisateur spécifié est introuvable`,
+    })
+    async createService(@Body() createServiceDto: CreateServiceDto) {
+        return this.serviceService.createService(createServiceDto)
+    }
+
+    @Delete(':id')
+    @ApiResponse({ status: 204, description: 'Service supprimé avec succès' })
+    @ApiResponse({
+        status: 404,
+        type: HttpExceptionResponse,
+        description: `${NotFoundException.name} => Aucun service trouvé avec cet ID`,
+    })
+    async deleteService(@Param('id') serviceId: number) {
+        await this.serviceService.deleteService([serviceId])
+    }
 }
