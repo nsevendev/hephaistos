96942a3689093576ed371a2a29a542017b2dbd04
BoissonMathis <math.boiss@laposte.net>
2024-10-18 16:13:26 +0200

    Ajout controller au module chat

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,10 +4,12 @@ import { ChatRepository } from './infra/chat.repository'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Chat } from './domaine/chat.entity'
 import { ChatRoomModule } from '../chat-room/chat-room.module'
+import { ChatController } from './app/chat.controller'
 
 @Module({
     imports: [TypeOrmModule.forFeature([Chat]), ChatRoomModule],
     providers: [ChatService, ChatRepository],
     exports: [ChatService, ChatRepository],
+    controllers: [ChatController],
 })
 export class ChatModule {}
