d432544a54858d833a43f9a6695165dfd75f0cd4
BoissonMathis <math.boiss@laposte.net>
2024-10-24 15:29:07 +0200

    création du module de l'entity car-for-sale-image

---
+++ src/car-for-sale-image/car-for-sale-image.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { CarForSaleImageService } from './app/car-for-sale-image.service'
+import { CarForSaleImageRepository } from './infra/car-for-sale-image.repository'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { CarForSaleImage } from './domaine/car-for-sale-image.entity'
+import { UserModule } from '../user/user.module'
+import { RoleModule } from '../role/role.module'
+import { CarForSaleModule } from '../car-for-sale/car-for-sale.module'
+// import { CarForSaleImageController } from './app/car-for-sale-image.controller'
+
+@Module({
+    imports: [TypeOrmModule.forFeature([CarForSaleImage]), UserModule, RoleModule, CarForSaleModule],
+    providers: [CarForSaleImageService, CarForSaleImageRepository],
+    exports: [CarForSaleImageService, CarForSaleImageRepository],
+    // controllers: [CarForSaleImageController],
+})
+export class CarForSaleImageModule {}
