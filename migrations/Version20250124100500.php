7827adf267f78d40192f0997a26769b79cc8e2f3
BoissonMathis <math.boiss@laposte.net>
2025-01-24 11:10:28 +0100

    add id for entity #49

---
+++ migrations/Version20250124100500.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250124100500 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE car_sales_entity (id VARCHAR(32) NOT NULL, info_description VARCHAR(32) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_7505A988B2DC6AD9 ON car_sales_entity (info_description)');
+        $this->addSql('COMMENT ON COLUMN car_sales_entity.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN car_sales_entity.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE car_sales_entity ADD CONSTRAINT FK_7505A988B2DC6AD9 FOREIGN KEY (info_description) REFERENCES info_description_model_entity (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE engine_remap_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE engine_remap_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE engine_remap_entity ALTER info_description TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE engine_remap_entity ALTER info_description TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE info_description_model_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE info_description_model_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE ping_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE ping_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE work_shop_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE work_shop_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE work_shop_entity ALTER info_description TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE work_shop_entity ALTER info_description TYPE VARCHAR(32)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE car_sales_entity DROP CONSTRAINT FK_7505A988B2DC6AD9');
+        $this->addSql('DROP TABLE car_sales_entity');
+        $this->addSql('ALTER TABLE ping_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE engine_remap_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE engine_remap_entity ALTER info_description TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE work_shop_entity ALTER id TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE work_shop_entity ALTER info_description TYPE VARCHAR(32)');
+        $this->addSql('ALTER TABLE info_description_model_entity ALTER id TYPE VARCHAR(32)');
+    }
+}
---
+++ src/Entity/LmQuatre/Type/LmQuatreId.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\LmQuatre\Type;
+
+use Heph\Entity\Shared\Type\Uid;
+
+class LmQuatreId extends Uid {}
