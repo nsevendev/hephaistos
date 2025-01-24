d1a88ed6c76c4fadc0e558d89d0fa01d21d2a8ab
BoissonMathis <math.boiss@laposte.net>
2025-01-24 06:19:24 +0100

    creation du faker InfoDescriptionModel pour test creation

---
+++ tests/Faker/Entity/InfoDescriptionModel/InfoDescriptionModelEntityFaker.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Faker\Entity\InfoDescriptionModel;
+
+use Heph\Entity\InfoDescriptionModel\InfoDescriptionModelEntity;
+
+final class InfoDescriptionModelEntityFaker
+{
+    public static function new(): InfoDescriptionModelEntity
+    {
+        return new InfoDescriptionModelEntity(
+            libelle: 'libellé test',
+            description: 'description test'
+        );
+    }
+}
