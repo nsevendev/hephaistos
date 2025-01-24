28ab5c0355e612db941b7dccf290d0c7561aa76e
BoissonMathis <math.boiss@laposte.net>
2025-01-24 10:34:46 +0100

    add test for entity #51

---
+++ tests/Unit/Entity/WorkShop/WorkShopEntityTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Unit\Entity\WorkShop;
+
+use DateTimeImmutable;
+use Heph\Entity\WorkShop\WorkShopEntity;
+use Heph\Entity\Shared\Type\Uid;
+use Heph\Infrastructure\Doctrine\Type\UidType;
+use Heph\Tests\Faker\Entity\WorkShop\WorkShopEntityFaker;
+use Heph\Tests\Unit\HephUnitTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(WorkShopEntity::class), CoversClass(Uid::class), CoversClass(UidType::class)]
+class WorkShopEntityTest extends HephUnitTestCase
+{
+    public function testEntityInitialization(): void
+    {
+        $WorkShopEntity = WorkShopEntityFaker::new();
+
+        self::assertInstanceOf(WorkShopEntity::class, $WorkShopEntity);
+        self::assertNotNull($WorkShopEntity->id());
+        self::assertInstanceOf(DateTimeImmutable::class, $WorkShopEntity->createdAt());
+        self::assertInstanceOf(DateTimeImmutable::class, $WorkShopEntity->updatedAt());
+        self::assertNotNull($WorkShopEntity->getInfoDescriptionModel());
+
+        $newDateUpdated = new DateTimeImmutable();
+        $WorkShopEntity->setUpdatedAt($newDateUpdated);
+
+        self::assertSame($newDateUpdated, $WorkShopEntity->updatedAt());
+    }
+}
