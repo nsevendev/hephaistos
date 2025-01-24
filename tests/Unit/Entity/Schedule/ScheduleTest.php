75ff51e62c25e889ee6e25b66d5aa80c2926fde6
BoissonMathis <math.boiss@laposte.net>
2025-01-24 16:38:50 +0100

    add test for entity #57

---
+++ tests/Unit/Entity/Schedule/ScheduleTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Unit\Entity\Schedule;
+
+use DateTimeImmutable;
+use Heph\Entity\Schedule\Schedule;
+use Heph\Entity\Shared\Type\Uid;
+use Heph\Infrastructure\Doctrine\Type\UidType;
+use Heph\Tests\Faker\Entity\Schedule\ScheduleFaker;
+use Heph\Tests\Unit\HephUnitTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Schedule::class), CoversClass(Uid::class), CoversClass(UidType::class)]
+class ScheduleTest extends HephUnitTestCase
+{
+    public function testEntityInitialization(): void
+    {
+        $schedule = ScheduleFaker::new();
+
+        self::assertInstanceOf(Schedule::class, $schedule);
+        self::assertNotNull($schedule->id());
+        self::assertSame('Monday', $schedule->day());
+        self::assertSame('09:00', $schedule->hoursStart());
+        self::assertSame('17:00', $schedule->hoursEnd());
+        self::assertInstanceOf(DateTimeImmutable::class, $schedule->createdAt());
+        self::assertInstanceOf(DateTimeImmutable::class, $schedule->updatedAt());
+
+        $newDateUpdated = new DateTimeImmutable();
+        $schedule->setUpdatedAt($newDateUpdated);
+
+        self::assertSame($newDateUpdated, $schedule->updatedAt());
+
+        $newDayUpdate = 'Tuesday';
+        $schedule->setDay($newDayUpdate);
+
+        self::assertSame($newDayUpdate, $schedule->day());
+
+        $newHoursStartUpdate = '08:00';
+        $schedule->setHoursStart($newHoursStartUpdate);
+
+        self::assertSame($newHoursStartUpdate, $schedule->hoursStart());
+
+        $newHoursEndUpdate = '16:00';
+        $schedule->setHoursEnd($newHoursEndUpdate);
+
+        self::assertSame($newHoursEndUpdate, $schedule->hoursEnd());
+    }
+
+    public function testEntityWithEmptyValues(): void
+    {
+        $schedule = ScheduleFaker::newWithEmptyValues();
+
+        self::assertInstanceOf(Schedule::class, $schedule);
+        self::assertNotNull($schedule->id());
+        self::assertSame('', $schedule->day());
+        self::assertSame('', $schedule->hoursStart());
+        self::assertSame('', $schedule->hoursEnd());
+        self::assertInstanceOf(DateTimeImmutable::class, $schedule->createdAt());
+        self::assertInstanceOf(DateTimeImmutable::class, $schedule->updatedAt());
+
+        $newDateUpdated = new DateTimeImmutable();
+        $schedule->setUpdatedAt($newDateUpdated);
+
+        self::assertSame($newDateUpdated, $schedule->updatedAt());
+    }
+}
