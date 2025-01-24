e3d898c9d1b20326a236563ab1bb69b75a2b456c
BoissonMathis <math.boiss@laposte.net>
2025-01-24 17:06:19 +0100

    add repository test for entity #50

---
+++ tests/Functional/Repository/CarSalesRepositoryTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Functional\Repository;
+
+use Doctrine\DBAL\Exception;
+use Heph\Entity\CarSales\CarSales;
+use Heph\Entity\Shared\Type\Uid;
+use Heph\Infrastructure\Doctrine\Type\UidType;
+use Heph\Repository\CarSales\CarSalesRepository;
+use Heph\Tests\Faker\Entity\CarSales\CarSalesFaker;
+use Heph\Tests\Functional\HephFunctionalTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use ReflectionException;
+
+#[
+    CoversClass(CarSalesRepository::class),
+    CoversClass(CarSales::class),
+    CoversClass(Uid::class),
+    CoversClass(UidType::class)
+]
+class CarSalesRepositoryTest extends HephFunctionalTestCase
+{
+    private CarSalesRepository $carSalesRepository;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $entityManager = $this->getEntityManager();
+        $entityManager->getConnection()->beginTransaction();
+
+        /** @var CarSalesRepository $repository */
+        $repository = self::getContainer()->get(CarSalesRepository::class);
+        $this->carSalesRepository = $repository;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function tearDown(): void
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        if ($conn->isTransactionActive()) {
+            $conn->rollBack();
+        }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testWeCanPersistAndFindCarSales(): void
+    {
+        $carSales = CarSalesFaker::new();
+
+        $this->persistAndFlush($carSales);
+
+        /** @var CarSales|null $found */
+        $found = $this->carSalesRepository->find($carSales->id());
+
+        self::assertNotNull($found, 'CarSales non trouvé en base alors qu’on vient de le créer');
+        self::assertInstanceOf(CarSales::class, $found);
+        self::assertNotNull($found->infoDescriptionModel());
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testWeCanUpdateCarSales(): void
+    {
+        $carSales = CarSalesFaker::new();
+
+        $this->persistAndFlush($carSales);
+
+        $infoDescriptionModel = $carSales->infoDescriptionModel();
+        $infoDescriptionModel->setLibelle('Nouveau libellé');
+        $infoDescriptionModel->setDescription('Nouvelle description');
+
+        $this->persistAndFlush($carSales);
+
+        /** @var CarSales|null $found */
+        $found = $this->carSalesRepository->find($carSales->id());
+
+        self::assertNotNull($found, 'CarSales non trouvé en base alors qu’on vient de le modifier');
+        self::assertSame('Nouveau libellé', $found->infoDescriptionModel()->libelle());
+        self::assertSame('Nouvelle description', $found->infoDescriptionModel()->description());
+    }
+}
