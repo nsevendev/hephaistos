92a5bc8b6a7ded541da23f6f769717ecf0ae420f
jha <j.haimez@vattenfall.fr>
2025-01-24 19:03:54 +0100

    chore(#48) add new CustomException for ping

---
+++ src/Infrastructure/ApiResponse/Exception/Custom/Ping/PingInvalidArgumentException.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Infrastructure\ApiResponse\Exception\Custom\Ping;
+
+use Heph\Infrastructure\ApiResponse\Exception\Custom\AbstractApiResponseException;
+use Heph\Infrastructure\ApiResponse\Exception\Error\Error;
+use Symfony\Component\HttpFoundation\Response;
+
+class PingInvalidArgumentException extends AbstractApiResponseException
+{
+    /**
+     * @param array<Error>|null $errors
+     */
+    public function __construct(
+        string $getMessage = '',
+        int $statusCode = 422,
+        ?array $errors = null,
+    ) {
+        $statusTexts = Response::$statusTexts;
+
+        if ('' === $getMessage && true === array_key_exists($statusCode, $statusTexts)) {
+            $getMessage = $statusTexts[$statusCode];
+        }
+
+        parent::__construct($getMessage, $statusCode, $errors);
+    }
+}
