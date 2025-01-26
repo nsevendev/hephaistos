c27fd96d04e326c1deb42e5ce983d7cc71b3ee3c
jha <j.haimez@vattenfall.fr>
2025-01-26 22:29:38 +0100

    chore(#48) create a get list ping with query

--- src/Entity/Ping/Dto/PingDto.php
+++ src/Entity/Ping/Dto/PingDto.php
@@ -4,7 +4,42 @@ declare(strict_types=1);
 
 namespace Heph\Entity\Ping\Dto;
 
+use Heph\Entity\Ping\Ping;
+
 class PingDto
 {
+    public function __construct(
+        public string $id,
+        public int $status,
+        public string $message,
+        public string $createdAt,
+        public string $updatedAt,
+    ) {}
 
+    public static function fromArray(Ping $data): self
+    {
+        return new self(
+            id: (string) $data->id(),
+            status: $data->status(),
+            message: $data->message(),
+            createdAt: $data->createdAt()->format('Y-m-d H:i:s'),
+            updatedAt: $data->updatedAt()->format('Y-m-d H:i:s'),
+        );
+    }
+
+    /**
+     * @param Ping[] $data
+     *
+     * @return PingDto[]
+     */
+    public static function toListPing(array $data): array
+    {
+        $listPing = [];
+
+        foreach ($data as $ping) {
+            $listPing[] = self::fromArray($ping);
+        }
+
+        return $listPing;
+    }
 }
--- src/Message/Query/Ping/GetListPingQuery.php
+++ src/Message/Query/Ping/GetListPingQuery.php
@@ -4,7 +4,4 @@ declare(strict_types=1);
 
 namespace Heph\Message\Query\Ping;
 
-class GetListPingQuery
-{
-
-}
+class GetListPingQuery {}
