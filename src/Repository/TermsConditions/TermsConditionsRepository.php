71a2e1b8833faab6c16f1a63a9f79f00fc6b3765
BoissonMathis <math.boiss@laposte.net>
2025-01-27 21:38:21 +0100

    add repository for entity #65

---
+++ src/Repository/TermsConditions/TermsConditionsRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Heph\Repository\TermsConditions;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Heph\Entity\TermsConditions\TermsConditions;
+
+/**
+ * @extends ServiceEntityRepository<TermsConditions>
+ */
+class TermsConditionsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TermsConditions::class);
+    }
+}
