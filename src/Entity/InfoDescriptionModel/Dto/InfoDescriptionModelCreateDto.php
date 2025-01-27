bab95d8f7ae0958cd772d0122d0b82f2b47110f8
BoissonMathis <math.boiss@laposte.net>
2025-01-27 23:05:10 +0100

    add create infoDescriptionModel Dto

---
+++ src/Entity/InfoDescriptionModel/Dto/InfoDescriptionModelCreateDto.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\InfoDescriptionModel\Dto;
+
+use Heph\Entity\Shared\ValueObject\LibelleValueObject;
+use Heph\Entity\Shared\ValueObject\DescriptionValueObject;
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class InfoDescriptionModelCreateDto
+{
+    public function __construct(
+        #[Assert\Valid]
+        private LibelleValueObject $libelle,
+        #[Assert\Valid]
+        private DescriptionValueObject $description,
+    ) {}
+
+    public static function new(string $libelle, string $description): self
+    {
+        return new self(
+            libelle: LibelleValueObject::fromValue($libelle),
+            description: DescriptionValueObject::fromValue($description),
+        );
+    }
+
+    public function libelle(): LibelleValueObject
+    {
+        return $this->libelle;
+    }
+
+    public function description(): DescriptionValueObject
+    {
+        return $this->description;
+    }
+}
