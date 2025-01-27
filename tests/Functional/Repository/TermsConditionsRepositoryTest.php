af50990ff60e1c14f118dce3c48d0e308284b5c4
BoissonMathis <math.boiss@laposte.net>
2025-01-27 21:42:46 +0100

    add tests repository for entity #64

---
+++ tests/Functional/Repository/TermsConditionsRepositoryTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Functional\Repository;
+
+use Doctrine\DBAL\Exception;
+use Heph\Entity\InfoDescriptionModel\InfoDescriptionModel;
+use Heph\Entity\TermsConditions\TermsConditions;
+use Heph\Repository\TermsConditions\TermsConditionsRepository;
+use Heph\Tests\Faker\Entity\TermsConditions\TermsConditionsFaker;
+use Heph\Tests\Functional\HephFunctionalTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use ReflectionException;
+
+#[
+    CoversClass(TermsConditionsRepository::class),
+    CoversClass(TermsConditions::class),
+    CoversClass(InfoDescriptionModel::class),
+]
+class TermsConditionsRepositoryTest extends HephFunctionalTestCase
+{
+    private TermsConditionsRepository $termsConditionsRepository;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $entityManager = $this->getEntityManager();
+        $entityManager->getConnection()->beginTransaction();
+
+        /** @var TermsConditionsRepository $repository */
+        $repository = self::getContainer()->get(TermsConditionsRepository::class);
+        $this->termsConditionsRepository = $repository;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function tearDown(): void
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        if ($conn->isTransactionActive()) {
+            $conn->rollBack();
+        }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testWeCanPersistAndFindTermsConditions(): void
+    {
+        $workShop = TermsConditionsFaker::new();
+
+        $this->persistAndFlush($workShop);
+
+        /** @var TermsConditions|null $found */
+        $found = $this->termsConditionsRepository->find($workShop->id());
+
+        // Vérifications
+        self::assertNotNull($found, 'WorkShop non trouvé en base alors qu’on vient de le créer');
+        self::assertInstanceOf(TermsConditions::class, $found);
+        self::assertNotNull($found->infoDescriptionModel());
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testWeCanUpdateTermsConditions(): void
+    {
+        $termsConditions = TermsConditionsFaker::new();
+
+        $this->persistAndFlush($termsConditions);
+
+        $infoDescriptionModel = $termsConditions->infoDescriptionModel();
+        $infoDescriptionModel->setLibelle('Nouveau libellé');
+        $infoDescriptionModel->setDescription('Nouvelle description');
+
+        $this->persistAndFlush($termsConditions);
+
+        /** @var TermsConditions|null $found */
+        $found = $this->termsConditionsRepository->find($termsConditions->id());
+
+        // Vérifications
+        self::assertNotNull($found, 'WorkShop non trouvé en base alors qu’on vient de le modifier');
+        self::assertSame('Nouveau libellé', $found->infoDescriptionModel()->libelle());
+        self::assertSame('Nouvelle description', $found->infoDescriptionModel()->description());
+    }
+}
