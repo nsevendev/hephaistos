ef1b4c22711f141eb8b0e1fbdaf1febd74c7f0ff
BoissonMathis <math.boiss@laposte.net>
2025-02-10 15:03:23 +0100

    add faker create dto for entity #60

---
+++ tests/Faker/Dto/EngineRemap/EngineRemapCreateDtoFaker.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Faker\Dto\EngineRemap;
+
+use Heph\Entity\EngineRemap\Dto\EngineRemapCreateDto;
+use Heph\Entity\InfoDescriptionModel\InfoDescriptionModel;
+use Symfony\Component\Uid\Uuid;
+use DateTimeImmutable;
+
+class EngineRemapCreateDtoFaker
+{
+    public static function new(): EngineRemapCreateDto
+    {
+        return new EngineRemapCreateDto(
+            id: Uuid::v7(),
+            infoDescriptionModel: new InfoDescriptionModel("libelle test", "description test"),
+            createdAt: new DateTimeImmutable(),
+            updatedAt: new DateTimeImmutable(),
+        );
+    }
+}
