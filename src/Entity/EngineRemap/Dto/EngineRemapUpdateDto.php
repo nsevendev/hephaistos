077032d582a101b2a4def8f179877cdd9d8f4a9e
BoissonMathis <math.boiss@laposte.net>
2025-02-14 16:04:40 +0100

    add update dto for entity #60

---
+++ src/Entity/EngineRemap/Dto/EngineRemapUpdateDto.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\EngineRemap\Dto;
+
+class EngineRemapUpdateDto
+{
+    public function __construct(
+        private readonly ?string $libelle,
+        private readonly ?string $description
+    ) {}
+
+    public function libelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function description(): ?string
+    {
+        return $this->description;
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            libelle: $data['libelle'] ?? null,
+            description: $data['description'] ?? null
+        );
+    }
+}
