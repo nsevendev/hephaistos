6028f774e94006bfb75c04a556565cd50aabcc8b
BoissonMathis <math.boiss@laposte.net>
2025-04-09 23:01:52 +0200

    refacto faker for entity TermsConditionsArticle

--- tests/Faker/Entity/TermsConditionsArticle/TermsConditionsArticleFaker.php
+++ tests/Faker/Entity/TermsConditionsArticle/TermsConditionsArticleFaker.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace Heph\Tests\Faker\Entity\TermsConditionsArticle;
 
 use Heph\Entity\TermsConditionsArticle\TermsConditionsArticle;
+use Heph\Entity\TermsConditionsArticle\ValueObject\TermsConditionsArticleArticle;
+use Heph\Entity\TermsConditionsArticle\ValueObject\TermsConditionsArticleTitle;
+use Heph\Infrastructure\ApiResponse\Exception\Custom\TermsConditionsArticle\TermsConditionsArticleInvalidArgumentException;
 use Heph\Tests\Faker\Entity\TermsConditions\TermsConditionsFaker;
 
 final class TermsConditionsArticleFaker
 {
+    /**
+     * @throws TermsConditionsArticleInvalidArgumentException
+     */
     public static function new(): TermsConditionsArticle
     {
         $termsConditions = TermsConditionsFaker::new();
 
         return new TermsConditionsArticle(
             termsConditions: $termsConditions,
-            title: 'titre test',
-            article: 'article test'
+            title: TermsConditionsArticleTitle::fromValue('titre test'),
+            article: TermsConditionsArticleArticle::fromValue('article test')
         );
     }
 }
