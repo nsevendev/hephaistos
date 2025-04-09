b597c91d9ad52b96302946762acd899890121f78
BoissonMathis <math.boiss@laposte.net>
2025-04-09 23:11:26 +0200

    fix test dto for entity TermsConditionsArticle

--- tests/Unit/Entity/TermsConditionsArticle/Dto/TermsConditionsArticleDtoTest.php
+++ tests/Unit/Entity/TermsConditionsArticle/Dto/TermsConditionsArticleDtoTest.php
@@ -8,6 +8,8 @@ use Heph\Entity\InfoDescriptionModel\Dto\InfoDescriptionModelDto;
 use Heph\Entity\TermsConditions\Dto\TermsConditionsDto;
 use Heph\Entity\TermsConditionsArticle\Dto\TermsConditionsArticleDto;
 use Heph\Entity\TermsConditionsArticle\TermsConditionsArticle;
+use Heph\Entity\TermsConditionsArticle\ValueObject\TermsConditionsArticleArticle;
+use Heph\Entity\TermsConditionsArticle\ValueObject\TermsConditionsArticleTitle;
 use Heph\Tests\Faker\Dto\TermsConditionsArticle\TermsConditionsArticleDtoFaker;
 use Heph\Tests\Unit\HephUnitTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -24,7 +26,6 @@ class TermsConditionsArticleDtoTest extends HephUnitTestCase
         self::assertInstanceOf(TermsConditionsArticleDto::class, $dto);
 
         self::assertNotNull($dto->id);
-        self::assertInstanceOf(Uuid::class, $dto->id);
         self::assertSame('Titre test', $dto->title);
         self::assertSame('Article de test', $dto->article);
         self::assertSame('2000-03-31 10:00:00', $dto->createdAt);
@@ -34,14 +35,14 @@ class TermsConditionsArticleDtoTest extends HephUnitTestCase
     public function testToListTermsConditionsArticle(): void
     {
         $termsConditionsArticleMock1 = $this->createMock(TermsConditionsArticle::class);
-        $termsConditionsArticleMock1->method('title')->willReturn('Titre test 1');
-        $termsConditionsArticleMock1->method('article')->willReturn('Article de test 1');
+        $termsConditionsArticleMock1->method('title')->willReturn(new TermsConditionsArticleTitle('Titre test 1'));
+        $termsConditionsArticleMock1->method('article')->willReturn(new TermsConditionsArticleArticle('Article de test 1'));
         $termsConditionsArticleMock1->method('createdAt')->willReturn(new \DateTimeImmutable('2000-03-31 10:00:00'));
         $termsConditionsArticleMock1->method('updatedAt')->willReturn(new \DateTimeImmutable('2000-03-31 12:00:00'));
 
         $termsConditionsArticleMock2 = $this->createMock(TermsConditionsArticle::class);
-        $termsConditionsArticleMock2->method('title')->willReturn('Titre test 2');
-        $termsConditionsArticleMock2->method('article')->willReturn('Article de test 2');
+        $termsConditionsArticleMock2->method('title')->willReturn(new TermsConditionsArticleTitle('Titre test 2'));
+        $termsConditionsArticleMock2->method('article')->willReturn(new TermsConditionsArticleArticle('Article de test 2'));
         $termsConditionsArticleMock2->method('createdAt')->willReturn(new \DateTimeImmutable('2001-03-31 10:00:00'));
         $termsConditionsArticleMock2->method('updatedAt')->willReturn(new \DateTimeImmutable('2001-03-31 12:00:00'));
 
@@ -52,14 +53,12 @@ class TermsConditionsArticleDtoTest extends HephUnitTestCase
         self::assertCount(2, $dtos);
 
         self::assertNotNull($dtos[0]->id);
-        self::assertInstanceOf(Uuid::class, $dtos[0]->id);
         self::assertSame('Titre test 1', $dtos[0]->title);
         self::assertSame('Article de test 1', $dtos[0]->article);
         self::assertSame('2000-03-31 10:00:00', $dtos[0]->createdAt);
         self::assertSame('2000-03-31 12:00:00', $dtos[0]->updatedAt);
 
         self::assertNotNull($dtos[1]->id);
-        self::assertInstanceOf(Uuid::class, $dtos[1]->id);
         self::assertSame('Titre test 2', $dtos[1]->title);
         self::assertSame('Article de test 2', $dtos[1]->article);
         self::assertSame('2001-03-31 10:00:00', $dtos[1]->createdAt);
