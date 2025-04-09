ee899a60f9469e681c57f1aca7b3cfd9008d6020
BoissonMathis <math.boiss@laposte.net>
2025-04-09 22:03:33 +0200

    refacto entity TermsConditions

--- src/Entity/TermsConditions/TermsConditions.php
+++ src/Entity/TermsConditions/TermsConditions.php
@@ -20,18 +20,57 @@ class TermsConditions
     #[ORM\Column(type: 'uuid', unique: true)]
     private Uuid $id;
 
+    /**
+     * @var Collection<int, TermsConditionsArticle> $listTermsConditionsArticle
+     */
+    #[ORM\OneToMany(mappedBy: 'termsConditions', targetEntity: TermsConditionsArticle::class, cascade: ['persist', 'remove'])]
+    private Collection $listTermsConditionsArticle;
+
+    #[ORM\Column(type: 'datetime_immutable', name: 'created_at', nullable: false)]
+    private DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable', name: 'updated_at', nullable: false)]
+    private DateTimeImmutable $updatedAt;
+
+    public function __construct(
+        #[ORM\OneToOne(targetEntity: InfoDescriptionModel::class, cascade: ['persist', 'remove'])]
+        #[ORM\JoinColumn(name: 'info_description_model', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
+        private InfoDescriptionModel $infoDescriptionModel,
+    ) {
+        $this->id = Uuid::v7();
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = $this->createdAt();
+
+        /* @var Collection<int, TermsConditionsArticle> */
+        $this->listTermsConditionsArticle = new ArrayCollection();
+    }
+
     public function id(): Uuid
     {
         return $this->id;
     }
 
-    #[ORM\OneToOne(targetEntity: InfoDescriptionModel::class, cascade: ['persist', 'remove'])]
-    #[ORM\JoinColumn(name: 'info_description_model', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
-    private InfoDescriptionModel $infoDescriptionModel;
-
     public function infoDescriptionModel(): InfoDescriptionModel
     {
         return $this->infoDescriptionModel;
+    }
+
+    /**
+     * @return Collection<int, TermsConditionsArticle>
+     */
+    public function listTermsConditionsArticle(): Collection
+    {
+        return $this->listTermsConditionsArticle;
+    }
+
+    public function createdAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function updatedAt(): DateTimeImmutable
+    {
+        return $this->updatedAt;
     }
 
     public function setInfoDescriptionModel(InfoDescriptionModel $infoDescriptionModel): void
@@ -40,18 +79,9 @@ class TermsConditions
         $this->updatedAt = new DateTimeImmutable();
     }
 
-    /**
-     * @var Collection<int, TermsConditionsArticle> $listTermsConditionsArticle
-     */
-    #[ORM\OneToMany(mappedBy: 'termsConditions', targetEntity: TermsConditionsArticle::class, cascade: ['persist', 'remove'])]
-    private Collection $listTermsConditionsArticle;
-
-    /**
-     * @return Collection<int, TermsConditionsArticle>
-     */
-    public function listTermsConditionsArticle(): Collection
+    public function setUpdatedAt(DateTimeImmutable $updatedAt): void
     {
-        return $this->listTermsConditionsArticle;
+        $this->updatedAt = $updatedAt;
     }
 
     public function addTermsConditionsArticle(TermsConditionsArticle $article): void
@@ -67,38 +97,5 @@ class TermsConditions
         if ($this->listTermsConditionsArticle->contains($article)) {
             $this->listTermsConditionsArticle->removeElement($article);
         }
-    }
-
-    #[ORM\Column(type: 'datetime_immutable', name: 'created_at', nullable: false)]
-    private DateTimeImmutable $createdAt;
-
-    public function createdAt(): DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    #[ORM\Column(type: 'datetime_immutable', name: 'updated_at', nullable: false)]
-    private DateTimeImmutable $updatedAt;
-
-    public function updatedAt(): DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(DateTimeImmutable $updatedAt): void
-    {
-        $this->updatedAt = $updatedAt;
-    }
-
-    public function __construct(
-        InfoDescriptionModel $infoDescriptionModel,
-    ) {
-        $this->id = Uuid::v7();
-        $this->infoDescriptionModel = $infoDescriptionModel;
-        $this->createdAt = new DateTimeImmutable();
-        $this->updatedAt = $this->createdAt();
-
-        /* @var Collection<int, TermsConditionsArticle> */
-        $this->listTermsConditionsArticle = new ArrayCollection();
     }
 }
