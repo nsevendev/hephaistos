3a6994d195b1369def2dccae7ae84d0fa1c76bae
BoissonMathis <math.boiss@laposte.net>
2025-04-12 11:32:54 +0200

    add dto for entity LmQuatre #62

---
+++ src/Entity/LmQuatre/Dto/LmQuatreDto.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\LmQuatre\Dto;
+
+use Heph\Entity\InfoDescriptionModel\Dto\InfoDescriptionModelDto;
+use Heph\Entity\LmQuatre\LmQuatre;
+
+class LmQuatreDto
+{
+    public function __construct(
+        public string $id,
+        public InfoDescriptionModelDto $infoDescriptionModel,
+        public string $owner,
+        public string $adresse,
+        public string $email,
+        public string $phoneNumber,
+        public string $createdAt,
+        public string $updatedAt,
+    ) {}
+
+    public static function fromArray(LmQuatre $data): self
+    {
+        return new self(
+            id: (string) $data->id(),
+            infoDescriptionModel: InfoDescriptionModelDto::fromEntity($data->infoDescriptionModel()),
+            owner: $data->owner()->value(),
+            adresse: $data->adresse()->value(),
+            email: $data->email()->value(),
+            phoneNumber: $data->phoneNumber()->value(),
+            createdAt: $data->createdAt()->format('Y-m-d H:i:s'),
+            updatedAt: $data->updatedAt()->format('Y-m-d H:i:s'),
+        );
+    }
+
+    /**
+     * @param LmQuatre[] $data
+     *
+     * @return LmQuatreDto[]
+     */
+    public static function toListLmQuatre(array $data): array
+    {
+        $listLmQuatre = [];
+
+        foreach ($data as $lmQuatre) {
+            $listLmQuatre[] = self::fromArray($lmQuatre);
+        }
+
+        return $listLmQuatre;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'infoDescriptionModel' => $this->infoDescriptionModel,
+            'owner' => $this->owner,
+            'adresse' => $this->adresse,
+            'email' => $this->email,
+            'phoneNumber' => $this->phoneNumber,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+        ];
+    }
+}
