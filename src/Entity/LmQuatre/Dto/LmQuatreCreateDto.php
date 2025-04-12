13245a8bb7428afdd33b1c4293a2695ec3d6405f
BoissonMathis <math.boiss@laposte.net>
2025-04-12 11:46:46 +0200

    add create dto for entity LmQuatre #62

---
+++ src/Entity/LmQuatre/Dto/LmQuatreCreateDto.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\LmQuatre\Dto;
+
+use Heph\Entity\InfoDescriptionModel\Dto\InfoDescriptionModelCreateDto;
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class LmQuatreCreateDto
+{
+    public function __construct(
+        #[Assert\NotBlank(message: "L'infoDescriptionModel est requis.")]
+        public InfoDescriptionModelCreateDto $infoDescriptionModel,
+        #[Assert\NotBlank(message: 'Le owner est requis.')]
+        #[Assert\Length(max: 50, maxMessage: 'Le owner doit contenir au plus {{ limit }} caractères.')]
+        public string $owner,
+        #[Assert\NotBlank(message: 'Le adresse est requis.')]
+        #[Assert\Length(max: 255, maxMessage: 'Le adresse doit contenir au plus {{ limit }} caractères.')]
+        public string $adresse,
+        #[Assert\NotBlank(message: 'Le email est requis.')]
+        #[Assert\Length(max: 50, maxMessage: 'Le email doit contenir au plus {{ limit }} caractères.')]
+        public string $email,
+        #[Assert\NotBlank(message: 'Le phoneNumber est requis.')]
+        #[Assert\Length(max: 50, maxMessage: 'Le phoneNumber doit contenir au plus {{ limit }} caractères.')]
+        public string $phoneNumber,
+    ) {}
+
+    public static function new(string $libelle, string $description, string $owner, string $adresse, string $email, string $phoneNumber): self
+    {
+        return new self(
+            infoDescriptionModel: InfoDescriptionModelCreateDto::new(
+                $libelle,
+                $description
+            ),
+            owner: $owner,
+            adresse: $adresse,
+            email: $email,
+            phoneNumber: $phoneNumber
+        );
+    }
+}
