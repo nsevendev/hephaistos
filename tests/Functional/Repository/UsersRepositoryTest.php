98291547f38648f70ba3f74065f19f864baea841
BoissonMathis <math.boiss@laposte.net>
2025-04-14 17:32:40 +0200

    add repository test for entity Users #47

---
+++ tests/Functional/Repository/UsersRepositoryTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Tests\Functional\Repository;
+
+use Doctrine\DBAL\Exception;
+use Heph\Entity\InfoDescriptionModel\InfoDescriptionModel;
+use Heph\Entity\Users\Users;
+use Heph\Entity\Users\ValueObject\UsersPassword;
+use Heph\Entity\Users\ValueObject\UsersRole;
+use Heph\Entity\Users\ValueObject\UsersUsername;
+use Heph\Infrastructure\Doctrine\Types\Users\UsersPasswordType;
+use Heph\Infrastructure\Doctrine\Types\Users\UsersRoleType;
+use Heph\Infrastructure\Doctrine\Types\Users\UsersUsernameType;
+use Heph\Repository\Users\UsersRepository;
+use Heph\Tests\Faker\Entity\Users\UsersFaker;
+use Heph\Tests\Functional\HephFunctionalTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use ReflectionException;
+
+#[
+    CoversClass(UsersRepository::class),
+    CoversClass(Users::class),
+    CoversClass(InfoDescriptionModel::class),
+    CoversClass(UsersUsername::class),
+    CoversClass(UsersUsernameType::class),
+    CoversClass(UsersPassword::class),
+    CoversClass(UsersPasswordType::class),
+    CoversClass(UsersRole::class),
+    CoversClass(UsersRoleType::class),
+]
+class UsersRepositoryTest extends HephFunctionalTestCase
+{
+    private UsersRepository $usersRepository;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $entityManager = $this->getEntityManager();
+        $entityManager->getConnection()->beginTransaction();
+
+        /** @var UsersRepository $repository */
+        $repository = self::getContainer()->get(UsersRepository::class);
+        $this->usersRepository = $repository;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function tearDown(): void
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        if ($conn->isTransactionActive()) {
+            $conn->rollBack();
+        }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testWeCanPersistAndFindUsers(): void
+    {
+        $users = UsersFaker::new();
+
+        $this->persistAndFlush($users);
+
+        /** @var Users|null $found */
+        $found = $this->usersRepository->find($users->id());
+
+        self::assertNotNull($found, 'Users non trouvé en base alors qu’on vient de le créer');
+        self::assertInstanceOf(Users::class, $found);
+        self::assertSame('username', $found->username()->value());
+        self::assertSame('password', $found->password()->value());
+        self::assertSame('admin', $found->role()->value());
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testWeCanUpdateUsers(): void
+    {
+        $users = UsersFaker::new();
+
+        $this->persistAndFlush($users);
+
+        $users->setUsername(new UsersUsername('new username'));
+        $users->setPassword(new UsersPassword('new passwod'));
+        $users->setRole(new UsersRole('employee'));
+
+        $this->persistAndFlush($users);
+
+        /** @var Users|null $found */
+        $found = $this->usersRepository->find($users->id());
+
+        self::assertNotNull($found, 'Users non trouvé en base alors qu’on vient de le modifier');
+        self::assertSame('new username', $found->username()->value());
+        self::assertSame('new passwod', $found->password()->value());
+        self::assertSame('employee', $found->role()->value());
+    }
+}
