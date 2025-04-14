870c35730d31e21e7f82c4dcba9ccbcee7e0a1eb
BoissonMathis <math.boiss@laposte.net>
2025-04-14 12:16:46 +0200

    add vo role for entity Users #47

---
+++ src/Entity/Users/ValueObject/UsersRole.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\Users\ValueObject;
+
+use Heph\Infrastructure\ApiResponse\Exception\Custom\Users\UsersInvalidArgumentException;
+use Heph\Infrastructure\ApiResponse\Exception\Error\Error;
+use JsonSerializable;
+use Stringable;
+
+readonly class UsersRole implements Stringable, JsonSerializable
+{
+    public function __construct(private string $value) {}
+
+    private const roles = [
+        'super_admin',
+        'admin',
+        'employee',
+    ];
+
+    /**
+     * @throws UsersInvalidArgumentException
+     */
+    public static function fromValue(string $value): self
+    {
+        $valueFormated = trim($value);
+
+        if ('' === $valueFormated) {
+            throw new UsersInvalidArgumentException(getMessage: 'Users role ne peux pas etre vide', errors: [Error::create(key: 'UsersRole', message: 'Users role ne peux pas etre vide')]);
+        }
+
+        if (!in_array($valueFormated, self::roles, true)) {
+            throw new UsersInvalidArgumentException(
+                getMessage: 'Role invalide',
+                errors: [Error::create('UsersRole', "Le role '{$valueFormated}' n'est pas autorisé.")]
+            );
+        }
+
+        return new self(value: $valueFormated);
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    public function jsonSerialize(): string
+    {
+        return $this->value;
+    }
+}
