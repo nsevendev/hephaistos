6eac1d227ccc9096cbd7445a37354a03beaa542e
BoissonMathis <math.boiss@laposte.net>
2025-04-14 09:33:59 +0200

    add update command handler for entity CarSales #61

--- src/Message/Command/CarSales/UpdateCarSalesCommand.php
+++ src/Message/Command/CarSales/UpdateCarSalesCommand.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Heph\Message\Command\CarSales;
 
-use Heph\Entity\CarSales\Dto\CarSalesCreateDto;
+use Heph\Entity\CarSales\Dto\CarSalesUpdateDto;
 
-class CreateCarSalesCommand
+class UpdateCarSalesCommand
 {
     public function __construct(
-        public CarSalesCreateDto $carSalesCreateDto,
+        public readonly CarSalesUpdateDto $carSalesUpdateDto,
+        public readonly string $id,
     ) {}
 }
