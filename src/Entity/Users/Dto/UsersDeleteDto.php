3fde076bd3dd6c6a0f4b0aa27b8d3d2890169738
BoissonMathis <math.boiss@laposte.net>
2025-04-15 22:35:42 +0200

    add delete dto for entity Users #47

---
+++ src/Entity/Users/Dto/UsersDeleteDto.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\Users\Dto;
+
+use Heph\Entity\Users\Users;
+
+class UsersPublishDeletedDto
+{
+    public function __construct(
+        public string $id,
+        public string $username,
+        public string $password,
+        public string $role,
+        public string $createdAt,
+        public string $updatedAt,
+    ) {}
+
+    public static function fromArray(Users $data): self
+    {
+        return new self(
+            id: (string) $data->id(),
+            username: $data->username()->value(),
+            password: $data->password()->value(),
+            role: $data->role()->value(),
+            createdAt: $data->createdAt()->format('Y-m-d H:i:s'),
+            updatedAt: $data->updatedAt()->format('Y-m-d H:i:s'),
+        );
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'password' => $this->password,
+            'role' => $this->role,
+            'delete' => 'true',
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+        ];
+    }
+}
