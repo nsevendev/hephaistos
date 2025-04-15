9cb0999e3a673503a1cf2efd6c640bebf6cefccb
BoissonMathis <math.boiss@laposte.net>
2025-04-15 12:56:40 +0200

    add bundle lexik_jwt for entity Users #47

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
