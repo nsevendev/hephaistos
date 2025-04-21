5d9c9a376a74041f46db427909ac3e64497ecb8b
BoissonMathis <math.boiss@laposte.net>
2025-04-21 10:35:15 +0200

    add create handler for entity TermsConditions #66

--- src/Entity/TermsConditions/Dto/TermsConditionsDto.php
+++ src/Entity/TermsConditions/Dto/TermsConditionsDto.php
@@ -26,6 +26,29 @@ class TermsConditionsDto
         );
     }
 
+    public static function fromArray(TermsConditions $data): self
+    {
+        return new self(
+            id: (string) $data->id(),
+            infoDescriptionModel: InfoDescriptionModelDto::fromEntity($data->infoDescriptionModel()),
+            createdAt: $data->createdAt()->format('Y-m-d H:i:s'),
+            updatedAt: $data->updatedAt()->format('Y-m-d H:i:s'),
+        );
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'infoDescriptionModel' => $this->infoDescriptionModel,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+        ];
+    }
+
     /**
      * @param TermsConditions[] $data
      *
---
+++ src/Message/Command/TermsConditions/CreateTermsConditionsHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Message\Command\TermsConditions;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Heph\Entity\TermsConditions\TermsConditions;
+use Heph\Entity\TermsConditions\Dto\TermsConditionsDto;
+use Heph\Entity\InfoDescriptionModel\InfoDescriptionModel;
+use Heph\Entity\Shared\ValueObject\DescriptionValueObject;
+use Heph\Entity\Shared\ValueObject\LibelleValueObject;
+use Heph\Infrastructure\ApiResponse\Exception\Custom\Mercure\MercureInvalidArgumentException;
+use Heph\Infrastructure\Mercure\MercurePublish;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler(bus: 'command.bus')]
+readonly class CreateTermsConditionsHandler
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private MercurePublish $mercurePublish,
+    ) {}
+
+    /**
+     * @throws MercureInvalidArgumentException
+     */
+    public function __invoke(CreateTermsConditionsCommand $command): void
+    {
+        $infoDescriptionModel = new InfoDescriptionModel(
+            libelle: LibelleValueObject::fromValue($command->termsConditionsCreateDto->infoDescriptionModel->libelle()),
+            description: DescriptionValueObject::fromValue($command->termsConditionsCreateDto->infoDescriptionModel->description()),
+        );
+        $this->entityManager->persist($infoDescriptionModel);
+
+        $termsConditions = new TermsConditions(
+            infoDescriptionModel: $infoDescriptionModel,
+        );
+
+        $this->entityManager->persist($termsConditions);
+        $this->entityManager->flush();
+
+        $termsConditionsDto = TermsConditionsDto::fromArray($termsConditions);
+
+        $this->mercurePublish->publish(
+            topic: '/terms-conditions-created',
+            data: $termsConditionsDto->toArray()
+        );
+    }
+}
