aa44c4198b527076907cf0119f007f264ec3f5c9
BoissonMathis <math.boiss@laposte.net>
2025-04-24 10:39:26 +0200

    fix create dto test and for entity TermsConditionsArticle #89

--- tests/Unit/Entity/TermsConditionsArticle/Dto/TermsConditionsArticleCreateDtoTest.php
+++ tests/Unit/Entity/TermsConditionsArticle/Dto/TermsConditionsArticleCreateDtoTest.php
@@ -9,7 +9,6 @@ use Heph\Entity\Shared\ValueObject\DescriptionValueObject;
 use Heph\Entity\Shared\ValueObject\LibelleValueObject;
 use Heph\Entity\TermsConditions\Dto\TermsConditionsCreateDto;
 use Heph\Entity\TermsConditionsArticle\Dto\TermsConditionsArticleCreateDto;
-use Heph\Tests\Faker\Dto\TermsConditions\TermsConditionsCreateDtoFaker;
 use Heph\Tests\Faker\Dto\TermsConditionsArticle\TermsConditionsArticleCreateDtoFaker;
 use Heph\Tests\Unit\HephUnitTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -19,24 +18,20 @@ class TermsConditionsArticleCreateDtoTest extends HephUnitTestCase
 {
     public function testTermsConditionsArticleCreateDto(): void
     {
-        $dto = TermsConditionsArticleCreateDtoFaker::new();
+        $dto = TermsConditionsArticleCreateDtoFaker::new('1234');
 
         self::assertNotNull($dto);
         self::assertInstanceOf(TermsConditionsArticleCreateDto::class, $dto);
 
         self::assertSame('titre test', $dto->title());
         self::assertSame('article test', $dto->article());
-
-        $termsConditionsDto = $dto->termsConditions();
-        self::assertNotNull($termsConditionsDto);
-        self::assertInstanceOf(TermsConditionsCreateDto::class, $termsConditionsDto);
+        self::assertSame('1234', $dto->termsConditionsId());
     }
 
     public function testTermsConditionsArticleCreateDtoWithFunctionNew(): void
     {
-        $termsConditions = TermsConditionsCreateDtoFaker::new();
         $dto = TermsConditionsArticleCreateDto::new(
-            termsConditions: $termsConditions,
+            termsConditionsId: '123456789',
             title: 'Titre manuel',
             article: 'Article manuel',
         );
@@ -46,9 +41,6 @@ class TermsConditionsArticleCreateDtoTest extends HephUnitTestCase
 
         self::assertSame('Titre manuel', $dto->title());
         self::assertSame('Article manuel', $dto->article());
-
-        $termsConditionsDto = $dto->termsConditions();
-        self::assertNotNull($termsConditionsDto);
-        self::assertInstanceOf(TermsConditionsCreateDto::class, $termsConditionsDto);
+        self::assertSame('123456789', $dto->termsConditionsId());
     }
 }
