b01fd2626b5480db896e46006ffa4eb8490f79bf
BoissonMathis <math.boiss@laposte.net>
2025-04-24 22:58:16 +0200

    add delete dto for entity TermsConditionsArticle #89

---
+++ src/Entity/TermsConditionsArticle/Dto/TermsConditionsArticleDeleteDto.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heph\Entity\TermsConditionsArticle\Dto;
+
+use Heph\Entity\TermsConditionsArticle\TermsConditionsArticle;
+
+class TermsConditionsArticlePublishDeletedDto
+{
+    public function __construct(
+        public string $id,
+        public string $title,
+        public string $article,
+        public string $createdAt,
+        public string $updatedAt,
+    ) {}
+
+    public static function fromArray(TermsConditionsArticle $data): self
+    {
+        return new self(
+            id: (string) $data->id(),
+            title: $data->title()->value(),
+            article: $data->article()->value(),
+            createdAt: $data->createdAt()->format('Y-m-d H:i:s'),
+            updatedAt: $data->updatedAt()->format('Y-m-d H:i:s'),
+        );
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'article' => $this->article,
+            'delete' => 'true',
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+        ];
+    }
+}
